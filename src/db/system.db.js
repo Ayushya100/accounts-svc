7e69225868b6064b08d2259dfaf6518f4208c21b
Ayushya <jaiswalayushya@gmail.com>
2025-02-19 06:47:28 +0530

    Updated query to convert timestamp data to date before returning data

--- src/db/system.db.js
+++ src/db/system.db.js
@@ -4,7 +4,7 @@ import { exec } from 'lib-finance-svc';
 
 const fetchServiceDetail = async(svcId = '') => {
     try {
-        let query = `SELECT id, microservice, environment, protocol, port, created_on, modified_on
+        let query = `SELECT id, microservice, environment, protocol, port, TO_CHAR(created_on, 'YYYY-MM-DD') created_on, TO_CHAR(modified_on, 'YYYY-MM-DD') modified_on
             FROM svc_configs
             WHERE is_deleted = false`;
         const params = [];
