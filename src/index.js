69e3587634159dd8770ea44494454ff24dc3860b
Ayushya <jaiswalayushya@gmail.com>
2025-04-19 17:37:08 +0530

    Initialized the service with the health-check API

---
+++ src/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import { Service } from 'finance-lib';
+import dotenv from 'dotenv';
+import { USERS_API, serviceConfig } from './constants.js';
+import routes from './routes/index.js';
+
+dotenv.config({
+  path: './env',
+});
+
+class AccountService extends Service {
+  registerServiceEndpoints() {
+    this.app.get(`${USERS_API}/health`, routes.healthCheck);
+  }
+}
+
+serviceConfig.HOST = process.env.HOST || serviceConfig.HOST;
+serviceConfig.PORT = process.env.PORT || serviceConfig.PORT;
+serviceConfig.PROTOCOL = process.env.PROTOCOL || serviceConfig.PROTOCOL;
+
+const service = new AccountService(serviceConfig, true);
+service.buildConnection();
+service.testConnection();
