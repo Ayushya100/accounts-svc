99a3af4b8efc08825474f42e3f90fdcb2e4ea084
Ayushya <jaiswalayushya@gmail.com>
2025-05-03 23:37:37 +0530

    Added get dashboard header by id API to the service

--- src/routes/dashboard-routes/index.js
+++ src/routes/dashboard-routes/index.js
@@ -2,8 +2,10 @@
 
 import registerDashboardHeader from './registerDashboardHeader.route.js';
 import getAllDashboardHeader from './getAllDashboardHeader.route.js';
+import getDashboardHeaderById from './getDashboardHeaderById.route.js';
 
 export default {
   registerDashboardHeader,
   getAllDashboardHeader,
+  getDashboardHeaderById,
 };
