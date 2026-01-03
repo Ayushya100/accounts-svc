3f2671bce7422fb94747c148ddebdc64bfc4ef0e
Ayushya <jaiswalayushya@gmail.com>
2026-01-03 22:41:46 +0530

    Added Verification successful mail to the verify-email API

--- src/controllers/account-controllers/verifyUser.controller.js
+++ src/controllers/account-controllers/verifyUser.controller.js
@@ -3,10 +3,37 @@
 import { _Error, _Response, convertPrettyStringToId, logger } from 'common-svc-lib';
 import { AccountDB } from '../../db/index.js';
 import { generateEmailVerificationCode } from './verificationCode.controller.js';
+import { requestEmailSend } from '../../utils/index.js';
 
 const log = logger('Controller: verify-user');
 
-const verifyUserToken = async (userId, token, userData) => {
+const sendVerificationConfirmationMail = (userId, userDtl, headers) => {
+  const userPayload = {
+    to: userDtl.email_id,
+    template: 'VERIFICATION_CONFIRM_MAIL',
+    data: {
+      id: userId,
+      role_code: userDtl.role_code,
+      first_name: userDtl.first_name,
+      last_name: userDtl.last_name,
+      username: userDtl.username,
+      email_id: userDtl.email_id,
+      is_verified: true,
+      login_type: userDtl.login_type
+    }
+  };
+  const userContext = {
+    userId: userId,
+    sessionId: headers['x-session-id'],
+    correlationId: headers['x-correlation-id']
+  };
+
+  requestEmailSend(userPayload, userContext);
+
+  log.success('Verification confirmation mail task submitted successfully');
+}
+
+const verifyUserToken = async (userId, token, userData, headers) => {
   try {
     log.info('Controller function to verify the user with the provided token operation initiated');
     userId = convertPrettyStringToId(userId);
@@ -27,12 +54,14 @@ const verifyUserToken = async (userId, token, userData) => {
     const currentTime = new Date(Date.now());
     if (currentTime > userMetaInfo.verification_token_exp) {
       log.error('Verification token has been expired');
-      await generateEmailVerificationCode(userId);
+      await generateEmailVerificationCode(userId, userData, headers);
       throw _Error(410, 'Verification code has been expired. New code will be send for verification');
     }
 
     log.info('Call db query to verify user');
     let emailVerificationResult = await AccountDB.verifyUserEmail(userId, currentTime);
+
+    sendVerificationConfirmationMail(userId, userData, headers);
 
     log.success('User email has been verified successfully');
     return _Response(200, 'Email is verified', {
