03cf50ebe583a12743fdf90ce3920a3f5a9add63
Ayushya <jaiswalayushya@gmail.com>
2026-01-05 14:11:31 +0530

    Added get user scope API

--- src/controllers/system-setup-controllers/getUserScope.controller.js
+++ src/controllers/system-setup-controllers/getUserScope.controller.js
@@ -29,4 +29,24 @@ const getUserScopeById = async (scopeId) => {
   }
 };
 
-export { getUserScopeById };
+const getAllUserScopes = async () => {
+  try {
+    log.info('Controller funciton to fetch all user scopes process initiated');
+    log.info('Call db query to fetch all user scopes');
+    let scopeDtl = await SystemDB.getUserScope();
+    if (scopeDtl.rowCount === 0) {
+      log.info('No user scope available to return');
+      return _Response(204, 'No user scope found', scopeDtl.rows);
+    }
+
+    scopeDtl = scopeDtl.rows;
+    formatResponseBody(scopeDtl, fieldMappings.userScopeFields);
+    log.success('User scopes fetched successfully');
+    return _Response(200, 'User scope found', scopeDtl);
+  } catch (err) {
+    log.error('Error occurred while trying to fetch all user scopes');
+    throw _Error(500, 'An error occurred while fetching user scopes', err);
+  }
+};
+
+export { getUserScopeById, getAllUserScopes };
